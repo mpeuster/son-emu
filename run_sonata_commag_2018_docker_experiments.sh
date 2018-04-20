b1e9138b36d78206394ff8a9813a8b97f0b33f9d
peusterm <manuel.peuster@uni-paderborn.de>
2018-04-20 12:39:19 +0200

    Docker ready to run
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

---
+++ run_sonata_commag_2018_docker_experiments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+set -e
+
+sudo python examples/evaluation_sonata_service_deployment_commag2018.py -t 1 -s examples/sonata_pkgs/eu.sonata-nfv.vfoward-2-1-test.0.2.1-docker.son -o result_docker_1pop_2vnf.csv -r 2
+
+sudo python examples/evaluation_sonata_service_deployment_commag2018.py -t 2 -s examples/sonata_pkgs/eu.sonata-nfv.vfoward-2-1-test.0.2.1-docker.son -o result_docker_2pop_2vnf.csv -r 2
+
+sudo python examples/evaluation_sonata_service_deployment_commag2018.py -t 1 -s examples/sonata_pkgs/eu.sonata-nfv.vfoward-8-1-test.0.8.1-docker.son -o result_docker_1pop_8vnf.csv -r 2
+
+sudo python examples/evaluation_sonata_service_deployment_commag2018.py -t 2 -s examples/sonata_pkgs/eu.sonata-nfv.vfoward-8-1-test.0.8.1-docker.son -o result_docker_2pop_8vnf.csv -r 2
